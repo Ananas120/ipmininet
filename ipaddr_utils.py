3aa8f610ed1df0725f606c6c16c5bb0fce995d6a
Quentin <quentinbananas@gmail.com>
2020-10-22 13:42:53 +0200

    subnet for link (code)

---
+++ ipaddr_utils.py
@@ -0,0 +1,63 @@
+def is_ipv6(addr):
+    return ':' in addr
+    
+def split_addr(address):
+    ipv6 = is_ipv6(address)
+    sep = ':' if ipv6 else '.'
+    
+    parts = []
+    for p in address.split(sep):
+        int_part = 0
+        if len(p) > 0:
+            int_part = int(p, 16 if ipv6 else 10)
+        parts.append(int_part)
+    return ipv6, parts
+
+def build_addr(parts, is_ipv6):
+    if not is_ipv6:
+        return '.'.join([str(p) for p in parts])
+    str_parts = []
+    for p in parts:
+        str_p = '' if p == 0 else hex(p)[2:]
+        str_parts.append(str_p)
+    
+    return ':'.join(str_parts)
+
+def format_prefixes(subnets):
+    return {
+        'ipv4' : {
+            name : config.get('ipv4', '/0').split('/')[0]
+            for name, config in subnets.items()
+        },
+        'ipv6' : {
+            name : config.get('ipv6', '/0').split('/')[0]
+            for name, config in subnets.items()
+        }
+    }
+
+def format_address(address_config, prefixes):    
+    addresses = []
+    if 'ipv4' in address_config:
+        addresses.append(address_config['ipv4'].format(** prefixes.get('ipv4', {})))
+    if 'ipv6' in address_config:
+        addresses.append(address_config['ipv6'].format(** prefixes.get('ipv6', {})))
+    
+    return addresses
+
+def create_subnets(subnet, n = 2):
+    if not isinstance(subnet, (list, tuple)): subnet = [subnet]
+    subnets = [[] for _ in range(n)]
+    for sub in subnet:
+        addr, mask_length = sub.split('/')
+        is_ipv6, addr_part = split_addr(addr)
+        
+        addr_host = 128 - int(mask_length) if is_ipv6 else 32 - int(mask_length)
+        max_addr = 2 ** addr_host
+        if n > max_addr:
+            raise ValueError("You want to create {} subnets with a mask of /{} which allows only {} subnets".format(n, mask_length, max_addr))
+        
+        for i in range(n):
+            new_addr = addr_part[:-1] + [addr_part[-1] + i]
+            new_addr = build_addr(new_addr, is_ipv6) + '/' + mask_length
+            subnets[i].append(new_addr)
+    return subnets
--- json_topo.py
+++ json_topo.py
@@ -7,31 +7,11 @@ from ipmininet.cli import IPCLI
 from ipmininet.iptopo import IPTopo
 from ipmininet.router.config import BGP, OSPF, OSPF6, RouterConfig, AF_INET6, set_rr, bgp_fullmesh, ebgp_session, SHARE
 
+from ipaddr_utils import format_prefixes, format_address, create_subnets
+
 _link_types = {
     'share' : SHARE
 }
-
-def _format_prefixes(subnets):
-    return {
-        'ipv4' : {
-            name : config.get('ipv4', '/0').split('/')[0]
-            for name, config in subnets.items()
-        },
-        'ipv6' : {
-            name : config.get('ipv6', '/0').split('/')[0]
-            for name, config in subnets.items()
-        }
-    }
-
-def _format_address(address_config, prefixes):    
-    addresses = []
-    if 'ipv4' in address_config:
-        addresses.append(address_config['ipv4'].format(** prefixes.get('ipv4', {})))
-    if 'ipv6' in address_config:
-        addresses.append(address_config['ipv6'].format(** prefixes.get('ipv6', {})))
-    
-    return addresses
-    
 
 class JSONTopo(IPTopo):
     def __init__(self, filename = 'topo.json', debug = False, *args, ** kwargs):
@@ -66,7 +46,7 @@ class JSONTopo(IPTopo):
         if isinstance(default_daemons, list):
             default_daemons = {d : {} for d in default_daemons}
         if isinstance(daemons, list): daemons = {d : {} for d in daemons}
-            
+        
         daemons = {** default_daemons, ** daemons}
         for d, d_config in daemons.items():
             if self.debug: print("Adding daemon {} with config {}".format(d, d_config))
@@ -76,7 +56,7 @@ class JSONTopo(IPTopo):
                 router.addDaemon(OSPF6)
             elif d == 'bgp':
                 if 'networks' in d_config:
-                    d_config['networks'] = _format_address(
+                    d_config['networks'] = format_address(
                         d_config['networks', self.__prefixes]
                     )
                 
@@ -91,7 +71,7 @@ class JSONTopo(IPTopo):
         config = json.loads(config)
         
         self.__subnets = config.get('subnets', {})
-        self.__prefixes = _format_prefixes(config.get('subnets', {}))
+        self.__prefixes = format_prefixes(config.get('subnets', {}))
         
         self._build_as(** config['AS'])
         self._build_links(** config.get('links', {}))
@@ -126,7 +106,7 @@ class JSONTopo(IPTopo):
             if 'lo_addresses' in r_config:
                 # option lo_addresses = [IPV6_ADDRESS, IPV4_ADDRESS] to set the loopback address of the router
                 # see https://ipmininet.readthedocs.io/en/latest/addressing.html
-                r_config['lo_addresses'] = _format_address(
+                r_config['lo_addresses'] = format_address(
                     r_config['lo_addresses'], self.__prefixes
                 )
             
@@ -179,19 +159,54 @@ class JSONTopo(IPTopo):
             as_1 = self.get_as_of(node)
             node = self.get(node)
             
+            config = {}
+            if isinstance(voisins, dict):
+                config = voisins
+                voisins = config.pop('voisins', [])
             if not isinstance(voisins, (list, tuple)): voisins = [voisins]
+            
+            ip1 = None
+            if 'ip' in config:
+                ip1 = format_address(config.pop('ip'), self.__prefixes)
+            
             for voisin in voisins:
-                config = {}
+                voisin_config = {}
                 if isinstance(voisin, list):
-                    voisin, config = voisin
+                    voisin, voisin_config = voisin
+                
                 as_2 = self.get_as_of(voisin)
                 voisin = self.get(voisin)
-                link_type = config.pop('link_type', 'share')
+                
+                link_type = voisin_config.pop('link_type', 'share')
+                
+                ip2 = None
+                if 'ip' in voisin_config:
+                    ip2 = format_address(
+                        voisin_config.pop('ip'), self.__prefixes
+                    )
+                
+                link_kwargs = {** config, ** voisin_config}
+                if ip1:
+                    link_kwargs.setdefault('param1', {})
+                    link_kwargs['param1']['ip'] = ip1
+                if ip2:
+                    link_kwargs.setdefault('param2', {})
+                    link_kwargs['param2']['i2'] = ip2
+                if 'subnet' in link_kwargs:
+                    link_kwargs.setdefault('param1', {})
+                    link_kwargs.setdefault('param2', {})
+                    
+                    subnet = link_kwargs.pop('subnet')
+                    subnet = _parse_address(subnet, self.__prefixes)
+                    subnet_1, subnet_2 = create_subnets(subnet)
+                    
+                    link_kwargs['param1']['ip'] = subnet_1
+                    link_kwargs['param2']['ip'] = subnet_1
                 
                 if self.debug:
-                    print("Adding link between {} and {} with config : {}".format(node, voisin, config))
+                    print("Adding link between {} and {} with config : {}".format(node, voisin, link_kwargs))
                 
-                link = self.addLink(node, voisin, ** config)
+                link = self.addLink(node, voisin, ** link_kwargs)
                 
                 if as_1 != as_2:
                     if self.debug:
@@ -208,7 +223,7 @@ class JSONTopo(IPTopo):
                     subnet_config.get('ipv6', None)
                 ))
             
-            addresses = _format_address(subnet_config, self.__prefixes)
+            addresses = format_address(subnet_config, self.__prefixes)
             
             nodes = [self.get(node_name) for node_name in subnet_config.get('nodes', [])]
             
